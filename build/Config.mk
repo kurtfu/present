89e5f8a4dfe3f91c8ae4073a4241ed1ca7bcb792
Furkan Kurt <kurtfu@yahoo.com>
2019-10-31 17:43:40 +0300

    feat: Add automatic file detection

--- build/Config.mk
+++ build/Config.mk
@@ -6,25 +6,29 @@
 # word. Whitespaces are not allowed. To use word sequences, separate the
 # words with underscore.
 
-PROJ      = present
+PROJ =
 
-# The tag describes the output type of the project. Before build the project,
-# output type must be specified.
+# The tag describes the output type of the project. Default value is 'EXEC'.
 # Supported types:
 # EXEC -> Executable output.
 # SLIB -> Static library.
 
-TYPE      = SLIB
+TYPE =
 
 #------------------------------------------------------------------------------
 # SOURCE CONFIGURATIONS
 #------------------------------------------------------------------------------
 
+# The tag describes the exclude files of the project. Any element of the tag
+# is not compiled. Multiple files could be added.
+
+EXL_FILE =
+
 # The tag specifies assembly file generation option. If the tag set as 'YES',
 # build system generates assembly files in addition to object files.
 # Otherwise, build system generates only object files.
 
-KEEP_ASM  = YES
+ASM_GEN  =
 
 #------------------------------------------------------------------------------
 # COMPILER AND LINKER CONFIGURATIONS
@@ -33,7 +37,7 @@ KEEP_ASM  = YES
 # The tag describes the search path for header files of the project. Multiple
 # paths could be added.
 
-INC_PATH  = ${PROJ_PATH}/include ${PROJ_PATH}/conf
+INC_PATH = ${PROJ_PATH}/include ${PROJ_PATH}/conf
 
 # The tag describes the search path for library files of the project. Multiple
 # paths could be added.
